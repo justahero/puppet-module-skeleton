2959646e5047526861a673ca0b36dbadab95e6cb
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2017-01-18 21:21:04 +0100

    Specs are run on all specified operating syste,s

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,8 +1,19 @@
 require 'spec_helper'
 
-describe 'test_module' do
-  context 'with defaults' do
-    it { is_expected.to compile }
-    it { is_expected.to contain_class('test_module') }
+describe 'test_module', type: :class do
+  on_supported_os.each do |os, facts|
+    describe "on #{os}" do
+      let(:facts) do
+        facts.merge({
+          'scenario' => '',
+          'common' => ''
+        })
+      end
+
+      context 'with defaults' do
+        it { is_expected.to compile }
+        it { is_expected.to contain_class('test_module') }
+      end
+    end
   end
 end
